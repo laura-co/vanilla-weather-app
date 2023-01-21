a33384387a6d5494bf10cb5feb76006d64d69073
laura-co <121612632+laura-co@users.noreply.github.com>
2023-01-21 12:18:08 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -45,7 +45,22 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
     let apiKey = "0581d02730e98a58a80fa1d4c2e31dcc";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Belfast&appid=${apiKey}&units=metric`;
-
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Belfast");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+
+
